ade900d1bcd4b87fd5f46238472b055e1b58d402
Raisndev <mseresagusil@gmail.com>
2022-08-09 18:18:18 +0200

    changed pokemon logo name

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="options__container">
 		<h2>Escoge tu</h2>
-		<img src="../assets/International_Pokémon_logo.png" alt="" />
+		<img src="../assets/pokemonLogo.png" alt="" />
 		<ul class="options">
 			<li v-for="pokemon in pokemons" :key="pokemon.id">
 				<a @click="$emit('selection', pokemon.id)">
